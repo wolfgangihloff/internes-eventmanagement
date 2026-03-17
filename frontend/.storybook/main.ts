300d483a142c78be404117011a86c8baf3c50339
Wolfgang Ihloff <wolfgang@ihloff.de>
2026-03-17 16:43:22 +0000

    Serve Storybook under /storybook/

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,8 +1,18 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
   stories: ['../src/docs/**/*.mdx'],
   addons: ['@storybook/addon-docs'],
   framework: '@storybook/react-vite',
+  async viteFinal(config, { configType }) {
+    if (configType !== 'PRODUCTION') {
+      return config;
+    }
+
+    return mergeConfig(config, {
+      base: '/storybook/',
+    });
+  },
 };
 export default config;
